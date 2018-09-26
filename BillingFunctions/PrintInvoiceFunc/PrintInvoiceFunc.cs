916d2dc128b0504ccd525519ce1a5af5b5e77faf
Robert Witkowski <robert.witkowski@altkom.pl>
2018-09-26 13:36:30 +0200

    Add PrintInvoiceFunc

---
+++ BillingFunctions/PrintInvoiceFunc/PrintInvoiceFunc.cs
@@ -0,0 +1,36 @@
+using Microsoft.Azure.WebJobs;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace BillingFunctions
+{
+    public static class PrintInvoiceFunc
+    {
+        [FunctionName("PrintInvoiceFunc")]
+        public static void Run(
+            [QueueTrigger("invoice-print-request")]InvoicePrintRequest printRequest,
+            Binder binder,
+            ILogger log)
+        {
+            var pdf = new InvoicePrinter().Print(printRequest.InvoiceToPrint);
+
+            //store result in blob
+            WriteOutBlobAsync(
+                binder,
+                $"Invoice_{printRequest.InvoiceToPrint.InvoiceNumber.Replace("/","_")}",
+                pdf);
+        }
+
+        private static async Task WriteOutBlobAsync(Binder binder, string title, byte[] doc)
+        {
+            using (var stream = await binder.BindAsync<Stream>(new BlobAttribute($"printouts/{title}.pdf", FileAccess.Write)))
+            {
+                using (var writer = new BinaryWriter(stream))
+                {
+                    writer.Write(doc);
+                }
+            }
+        }
+    }
+}
