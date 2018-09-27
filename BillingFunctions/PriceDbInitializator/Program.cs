8cb704097b987cdcf0e917423794bafeab543a03
Robert Witkowski <robert.witkowski@altkom.pl>
2018-09-27 15:16:48 +0200

    Add PriceDbInitializator project.

---
+++ BillingFunctions/PriceDbInitializator/Program.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Azure.Documents.Client;
+using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace PriceDbInitializator
+{
+    class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        static void Main(string[] args)
+        {
+            string configFile = GetConfigFile(args);
+            var builder = new ConfigurationBuilder()
+                        .AddJsonFile(configFile);
+
+            Configuration = builder.Build();
+
+            var client = new DocumentClient(
+                new Uri(Configuration["priceDbUrl"]),
+                Configuration["priceDbAuthKey"]);
+
+            AddDoc(client);
+            GetDocument(client);
+
+            Console.ReadLine();
+        }
+
+        static void GetDocument(DocumentClient client)
+        {
+            var priceList = client.CreateDocumentQuery<PriceList>(
+                UriFactory.CreateDocumentCollectionUri("crm", "prices"))
+                .Where(p => p.CustomerCode == "ASC");
+
+            foreach (var pl in priceList)
+            {
+                Console.WriteLine($"{pl.CustomerCode}");
+                foreach (var pli in pl.Prices)
+                {
+                    Console.WriteLine($"{pli.ProductCode} {pli.Price}");
+                }
+            }
+
+        }
+
+        static void AddDoc(DocumentClient client)
+        {
+            var price = new PriceList
+            {
+                CustomerCode = "ASC",
+                Prices = new List<PriceListItem>
+                {
+                    new PriceListItem
+                    {
+                        ProductCode = "A",
+                        Gender = Gender.Male,
+                        AgeFrom = 0,
+                        AgeTo = 65,
+                        Price = 200
+                    },
+                    new PriceListItem
+                    {
+                        ProductCode = "A",
+                        Gender = Gender.Male,
+                        AgeFrom = 66,
+                        AgeTo = 100,
+                        Price = 250
+                    },
+                    new PriceListItem
+                    {
+                        ProductCode = "A",
+                        Gender = Gender.Female,
+                        AgeFrom = 0,
+                        AgeTo = 65,
+                        Price = 190
+                    },
+                    new PriceListItem
+                    {
+                        ProductCode = "A",
+                        Gender = Gender.Female,
+                        AgeFrom = 66,
+                        AgeTo = 100,
+                        Price = 240
+                    },
+
+                    new PriceListItem
+                    {
+                        ProductCode = "B",
+                        Gender = Gender.Male,
+                        AgeFrom = 0,
+                        AgeTo = 65,
+                        Price = 200
+                    },
+                    new PriceListItem
+                    {
+                        ProductCode = "B",
+                        Gender = Gender.Male,
+                        AgeFrom = 66,
+                        AgeTo = 100,
+                        Price = 250
+                    },
+                    new PriceListItem
+                    {
+                        ProductCode = "B",
+                        Gender = Gender.Female,
+                        AgeFrom = 0,
+                        AgeTo = 65,
+                        Price = 190
+                    },
+                    new PriceListItem
+                    {
+                        ProductCode = "B",
+                        Gender = Gender.Female,
+                        AgeFrom = 66,
+                        AgeTo = 100,
+                        Price = 240
+                    },
+                }
+            };
+
+            client.CreateDocumentAsync(
+                UriFactory.CreateDocumentCollectionUri("crm", "prices"),
+                price);
+        }
+
+        private static string GetConfigFile(string[] args)
+        {
+            return args.Length > 0 && args[0] != null ? args[0] : "local.appsettings.json";
+        }
+    }
+
+    class PriceList
+    {
+        public string CustomerCode { get; set; }
+        public IList<PriceListItem> Prices { get; set; }
+    }
+
+    class PriceListItem
+    {
+        public string ProductCode { get; set; }
+        public Gender Gender { get; set; }
+        public int AgeFrom { get; set; }
+        public int AgeTo { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
